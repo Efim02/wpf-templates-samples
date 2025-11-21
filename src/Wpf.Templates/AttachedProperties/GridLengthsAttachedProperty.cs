5c1ab74e9d9d904443d5371cbaf3a0da8eff3a09
Efim <49785820+Efim02@users.noreply.github.com>
2025-11-21 13:58:31 +0700

    Update GridLengthsAttachedProperty.cs

--- src/Wpf.Templates/AttachedProperties/GridLengthsAttachedProperty.cs
+++ src/Wpf.Templates/AttachedProperties/GridLengthsAttachedProperty.cs
@@ -1,129 +1,139 @@
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-namespace Wpf.Templates.AttachedProperties
+namespace Nst.UI.AttachedProperties;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+/// <summary>
+/// Прикрепляемое свойство для <see cref="Grid" /> с целью упрощенного назначения колонок, строк.
+/// </summary>
+// ReSharper disable once ClassNeverInstantiated.Global Объект создается в XAML.
+public class GridAp : UIElement
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Globalization;
-    using System.Linq;
-    using System.Windows;
-    using System.Windows.Controls;
+    /// <summary>
+    /// Длины колонок.
+    /// </summary>
+    public static readonly DependencyProperty ColumnLengthsProperty =
+        DependencyProperty.RegisterAttached("ColumnLengths",
+            typeof(GridLengths),
+            typeof(Grid),
+            new FrameworkPropertyMetadata(default(GridLengths),
+                OnColumnLengthsChanged));
 
     /// <summary>
-    /// Прикрепляемое свойство для <see cref="Grid" /> с целью упрощенного назначения колонок, строк.
+    /// Длины строк.
     /// </summary>
-    // ReSharper disable once ClassNeverInstantiated.Global Объект создается в XAML.
-    public class GridAp : UIElement
+    public static readonly DependencyProperty RowLengthsProperty =
+        DependencyProperty.RegisterAttached("RowLengths",
+            typeof(GridLengths),
+            typeof(Grid),
+            new FrameworkPropertyMetadata(default(GridLengths),
+                OnRowLengthsChanged));
+
+    /// <summary>
+    /// Получает длины колонок.
+    /// </summary>
+    public static GridLengths GetColumnLengths(Grid element) => (GridLengths)element.GetValue(ColumnLengthsProperty);
+
+    /// <summary>
+    /// Получает длины строк.
+    /// </summary>
+    public static GridLengths GetRowLengths(Grid element) => (GridLengths)element.GetValue(RowLengthsProperty);
+
+    /// <summary>
+    /// Устанавливает длины колонок.
+    /// </summary>
+    public static void SetColumnLengths(Grid element, GridLengths value)
     {
-        public static readonly DependencyProperty ColumnLengthsProperty =
-            DependencyProperty.RegisterAttached("ColumnLengths",
-                typeof(GridLengths),
-                typeof(Grid),
-                new FrameworkPropertyMetadata(default(GridLengths),
-                    OnColumnLengthsChanged));
+        element.SetValue(ColumnLengthsProperty, value);
+    }
 
-        public static readonly DependencyProperty RowLengthsProperty =
-            DependencyProperty.RegisterAttached("RowLengths",
-                typeof(GridLengths),
-                typeof(Grid),
-                new FrameworkPropertyMetadata(default(GridLengths),
-                    OnRowLengthsChanged));
+    /// <summary>
+    /// Устанавливает длины строк.
+    /// </summary>
+    public static void SetRowLengths(Grid element, GridLengths value)
+    {
+        element.SetValue(RowLengthsProperty, value);
+    }
 
-        public static GridLengths GetColumnLengths(Grid element)
+    /// <summary>
+    /// Обработка изменения ширин колонок.
+    /// </summary>
+    private static void OnColumnLengthsChanged(DependencyObject d, DependencyPropertyChangedEventArgs ev)
+    {
+        var grid = (Grid)d;
+        var newValue = (GridLengths)ev.NewValue;
+        grid.ColumnDefinitions.Clear();
+
+        if (newValue == null)
+            return;
+
+        foreach (var gridLength in newValue)
         {
-            return (GridLengths)element.GetValue(ColumnLengthsProperty);
-        }
-
-        public static GridLengths GetRowLengths(Grid element)
-        {
-            return (GridLengths)element.GetValue(RowLengthsProperty);
-        }
-
-        public static void SetColumnLengths(Grid element, GridLengths value)
-        {
-            element.SetValue(ColumnLengthsProperty, value);
-        }
-
-        public static void SetRowLengths(Grid element, GridLengths value)
-        {
-            element.SetValue(RowLengthsProperty, value);
-        }
-
-        /// <summary>
-        /// Обработка изменения ширин колонок.
-        /// </summary>
-        private static void OnColumnLengthsChanged(DependencyObject d, DependencyPropertyChangedEventArgs ev)
-        {
-            var grid = (Grid)d;
-            var newValue = (GridLengths)ev.NewValue;
-            grid.ColumnDefinitions.Clear();
-
-            if (newValue == null)
-                return;
-
-            foreach (var gridLength in newValue)
+            var columnDefinition = new ColumnDefinition
             {
-                var columnDefinition = new ColumnDefinition
-                {
-                    Width = gridLength
-                };
-                grid.ColumnDefinitions.Add(columnDefinition);
-            }
-        }
-
-        /// <summary>
-        /// Обработка изменения высот строк.
-        /// </summary>
-        private static void OnRowLengthsChanged(DependencyObject d, DependencyPropertyChangedEventArgs ev)
-        {
-            var grid = (Grid)d;
-            var newValue = (GridLengths)ev.NewValue;
-            grid.RowDefinitions.Clear();
-
-            if (newValue == null)
-                return;
-
-            foreach (var gridLength in newValue)
-            {
-                var rowDefinition = new RowDefinition
-                {
-                    Height = gridLength
-                };
-                grid.RowDefinitions.Add(rowDefinition);
-            }
+                Width = gridLength,
+            };
+            grid.ColumnDefinitions.Add(columnDefinition);
         }
     }
 
     /// <summary>
-    /// Список длин для <see cref="Grid" />.
+    /// Обработка изменения высот строк.
     /// </summary>
-    [TypeConverter(typeof(GridLengthsConverter))]
-    public class GridLengths : List<GridLength>
+    private static void OnRowLengthsChanged(DependencyObject d, DependencyPropertyChangedEventArgs ev)
     {
-    }
+        var grid = (Grid)d;
+        var newValue = (GridLengths)ev.NewValue;
+        grid.RowDefinitions.Clear();
 
-    /// <summary>
-    /// Конвертер для преобразования <see cref="GridLengths " /> из строки.
-    /// </summary>
-    public class GridLengthsConverter : TypeConverter
-    {
-        private static readonly GridLengthConverter GridLengthConverter = new GridLengthConverter();
-        private static readonly string[] Separators = { ",", ", ", " " };
+        if (newValue == null)
+            return;
 
-        /// <inheritdoc />
-        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        foreach (var gridLength in newValue)
         {
-            if (!(value is string row))
-                throw new ArgumentException($"Параметр {nameof(value)} должен иметь тип {typeof(string)}");
-
-            var values = row.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
-            if (!values.Any())
-                return new GridLengths();
-
-            var listColumnDefinitions = new GridLengths();
-            listColumnDefinitions.AddRange(values.Select(v => GridLengthConverter.ConvertFrom(v)).Cast<GridLength>());
-
-            return listColumnDefinitions;
+            var rowDefinition = new RowDefinition
+            {
+                Height = gridLength,
+            };
+            grid.RowDefinitions.Add(rowDefinition);
         }
+    }
+}
+
+/// <summary>
+/// Список длин для <see cref="Grid" />.
+/// </summary>
+[TypeConverter(typeof(GridLengthsConverter))]
+public class GridLengths : List<GridLength>
+{
+}
+
+/// <summary>
+/// Конвертер для преобразования <see cref="GridLengths " /> из строки.
+/// </summary>
+public class GridLengthsConverter : TypeConverter
+{
+    private static readonly GridLengthConverter GridLengthConverter = new GridLengthConverter();
+    private static readonly string[] Separators = { ",", ", ", " " };
+
+    /// <inheritdoc />
+    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+    {
+        if (!(value is string row))
+            throw new ArgumentException($"Параметр {nameof(value)} должен иметь тип {typeof(string)}");
+
+        var values = row.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+        if (!values.Any())
+            return new GridLengths();
+
+        var listColumnDefinitions = new GridLengths();
+        listColumnDefinitions.AddRange(values.Select(v => GridLengthConverter.ConvertFrom(v)).Cast<GridLength>());
+
+        return listColumnDefinitions;
     }
 }
